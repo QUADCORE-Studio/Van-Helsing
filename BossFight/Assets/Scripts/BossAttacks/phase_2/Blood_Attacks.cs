f4895969fde6e71599e0e10e31eb611434724af1
isaac1441 <issaac1414@gmail.com>
2025-07-21 23:30:02 -0500

    blood attacks & prefabs

--- BossFight/Assets/Scripts/BossAttacks/phase_2/Blood_Attacks.cs
+++ BossFight/Assets/Scripts/BossAttacks/phase_2/Blood_Attacks.cs
@@ -5,7 +5,11 @@ public class Blood_Attacks : MonoBehaviour
 {
     public Animator animator;
     public InputAction inputTrig;
-
+    public GameObject arrow;
+    public GameObject beam;
+    public int projectileCount = 8;
+    public float projectileSpeed = 5f;
+    private Transform target;
     private void OnEnable()
     {
         inputTrig.Enable();
@@ -18,17 +22,58 @@ public class Blood_Attacks : MonoBehaviour
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        Debug.Log("I belong to: " + gameObject.name);
 
+        if (transform.parent != null)
+        {
+            Debug.Log("My parent is: " + transform.parent.name);
+        }
+        animator = GetComponent<Animator>();
+        target = transform;
     }
 
     void Update()
     {
         if (inputTrig.WasPressedThisFrame())
         {
-            animator.SetTrigger("beam_attack");
-            Debug.Log(animator);
-            Debug.Log("pressed e");
+            circle_attack();
         }
+        if (inputTrig.WasPressedThisFrame())
+        {
+            beam_attack();
+        }
+    }
+
+    void circle_attack()
+    {
+        float angleStep = 360f / projectileCount;
+
+        for (int i = 0; i < projectileCount; i++)
+        {
+            float angle = i * angleStep;
+            float rad = angle * Mathf.Deg2Rad;
+
+            Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)).normalized;
+
+            GameObject projectile = Instantiate(arrow, target.position, Quaternion.identity);
+            projectile.transform.right = direction;
+
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = direction * projectileSpeed;
+
+            Destroy(projectile, 2f);
+        }
+
+    }
+    void beam_attack()
+    {
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+        GameObject projectile = Instantiate(beam, target.position, Quaternion.identity);
+            projectile.transform.right = direction;
+
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.linearVelocity = direction * projectileSpeed;
+
+            Destroy(projectile, 5f);
     }
 }
