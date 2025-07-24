8e8adfa8e068d088cb6fbfeb9fd2a0a0e145228b
isaac1441 <issaac1414@gmail.com>
2025-07-24 01:16:29 -0500

    blood attcaks & animation, teleporting works

--- BossFight/Assets/Scripts/BossAttacks/phase_2/teleporter.cs
+++ BossFight/Assets/Scripts/BossAttacks/phase_2/teleporter.cs
@@ -14,16 +14,9 @@ public class teleporter : MonoBehaviour
     private GameObject exit;
     private Animator entrance_anim;
     private Animator exit_anim;
-    public void OnAnimationEnd()
-    {
-        Destroy(gameObject, 2f);
-    }
-
-
-
+    public float offset;
     void Start()
     {
-
         StartRepeating();
     }
 
@@ -39,16 +32,19 @@ public class teleporter : MonoBehaviour
         Vector2 randomPoint = UnityEngine.Random.insideUnitCircle * radius;
         Vector2 newPos = new Vector2(centerPoint.position.x, centerPoint.position.y) + new Vector2(randomPoint.x, randomPoint.y);
 
-        GameObject entrance = Instantiate(teleporter_1, transform, true);
+        GameObject entrance = Instantiate(teleporter_1, transform.position, Quaternion.identity);
 
         transform.position = newPos;
-        GameObject exit = Instantiate(teleporter_2, transform, true);
-        
+        Vector2 spawnPos = (Vector2)transform.position + new Vector2(0, offset);
+        GameObject exit = Instantiate(teleporter_2, spawnPos, Quaternion.identity);
+
         entrance_anim = entrance.GetComponent<Animator>();
         exit_anim = exit.GetComponent<Animator>();
-
         entrance_anim.SetTrigger("tele");
+        Destroy(entrance, 1f);
+
         exit_anim.SetTrigger("tele");
+        Destroy(exit, 1f);
     }
     //show teleportable radius in editor 
     void OnDrawGizmosSelected()
@@ -60,7 +56,7 @@ public class teleporter : MonoBehaviour
         }
     }
 
-    public float repeatRate = 4f;
+    public float repeatRate = 7f;
     private Coroutine repeatCoroutine;
     // Start the repeating coroutine to teleport 
     public void StartRepeating()
