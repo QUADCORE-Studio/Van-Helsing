ed53b2daf9b659e46c1371399bd2541e2141bd4f
KrystalGtz <krystaljadegutierrez@gmail.com>
2025-07-26 04:08:13 -0500

    Changed Interact button to not have hold

--- BossFight/Assets/Scripts/Player/PlayerCombat.cs
+++ BossFight/Assets/Scripts/Player/PlayerCombat.cs
@@ -1,47 +1,47 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
 
-public class PlayerCombat : MonoBehaviour
-{
-    public Animator animator;
-    private PlayerControls controls;
-    public Transform attackPoint;
-    public GameObject slashPrefab;
+// public class PlayerCombat : MonoBehaviour
+// {
+//     public Animator animator;
+//     private PlayerControls controls;
+//     public Transform attackPoint;
+//     public GameObject slashPrefab;
 
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
-        controls = new PlayerControls();
-    }
+//     void Awake()
+//     {
+//         animator = GetComponent<Animator>();
+//         controls = new PlayerControls();
+//     }
 
-    public void Attack()
-    {
-        animator.Play("AttackPlayer");
+//     public void Attack()
+//     {
+//         animator.Play("AttackPlayer");
 
-        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-        mouseWorldPos.z = 0;
+//         Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+//         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+//         mouseWorldPos.z = 0;
 
-        Vector2 direction = (mouseWorldPos - attackPoint.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//         Vector2 direction = (mouseWorldPos - attackPoint.position).normalized;
+//         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        Instantiate(slashPrefab, attackPoint.position, Quaternion.Euler(0, 0, angle));
+//         Instantiate(slashPrefab, attackPoint.position, Quaternion.Euler(0, 0, angle));
 
-    }
+//     }
 
-    void OnEnable()
-    {
-        controls.Enable();
-        controls.Player.Attack.performed += ctx => Attack();
-    }
+//     void OnEnable()
+//     {
+//         controls.Enable();
+//         controls.Player.Attack.performed += ctx => Attack();
+//     }
 
-    void OnDisable()
-    {
-        controls.Player.Attack.performed -= ctx => Attack();
-        controls.Disable();
-    }
+//     void OnDisable()
+//     {
+//         controls.Player.Attack.performed -= ctx => Attack();
+//         controls.Disable();
+//     }
 
 
-    //dodge logic
+//     //dodge logic
 
-}
+// }
